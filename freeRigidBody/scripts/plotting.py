2b6f13067574edae76b0ac73f91481f2ef9551d9
Davide Murari <53520360+davidemurari@users.noreply.github.com>
2024-10-09 11:35:07 +0100

    Update plotting.py

--- freeRigidBody/scripts/plotting.py
+++ freeRigidBody/scripts/plotting.py
@@ -14,27 +14,20 @@ def plot(y0,N,h,sol_LE,sol_EE):
     matplotlib.rcParams['font.size']= 45
     matplotlib.rcParams['font.family']= 'ptm' #'Times New Roman
 
-    # Assuming sol_IM and sol_EE are defined earlier in your code
-
-    # Create a figure with 1 row and 2 columns of subplots
     fig = plt.figure(figsize=(14, 7), dpi=300)
     gs = gridspec.GridSpec(2, 2, width_ratios=[2, 1], height_ratios=[1, 1],hspace=0.5)
 
-    # First subplot for the 3D plot (Implicit Midpoint and Explicit Euler)
     ax1 = fig.add_subplot(gs[:, 0], projection='3d')  # 1 row, 2 cols, 1st subplot
 
-    # Plot the curve from your data
     ax1.plot(sol_LE[0], sol_LE[1], sol_LE[2], 'b-', label='Lie Euler')
     ax1.plot(sol_EE[0], sol_EE[1], sol_EE[2], 'k--', label='Explicit Euler')
 
-    # Generate points for a unit sphere
     u = np.linspace(0, 2 * np.pi, 100)
     v = np.linspace(0, np.pi, 100)
     x = 1 * np.outer(np.cos(u), np.sin(v))
     y = 1 * np.outer(np.sin(u), np.sin(v))
     z = 1 * np.outer(np.ones(np.size(u)), np.cos(v))
 
-    # Plot the unit sphere on the first subplot
     ax1.plot_surface(x, y, z, color='r', alpha=0.1)
     ax1.set_xlabel(r'$x_1$',fontsize=20)
     ax1.set_ylabel(r'$x_2$',fontsize=20)
@@ -64,11 +57,8 @@ def plot(y0,N,h,sol_LE,sol_EE):
 
     ax1.axis('equal')
 
-    # Second subplot (Placeholder for another plot)
     ax2_0 = fig.add_subplot(gs[0,1])  # 1 row, 2 cols, 2nd subplot
 
-    # Example plot for the second subplot
-    # Replace this with your actual plotting code for the second plot
     x_EE = np.linspace(0, N*h, N+1)
     x_IM = np.linspace(0, N*h, N+1)
     ax2_0.plot(x_IM, np.abs(H(sol_LE)-H(y0)), 'b-',label="Lie Euler")
@@ -81,8 +71,6 @@ def plot(y0,N,h,sol_LE,sol_EE):
 
     ax2_1 = fig.add_subplot(gs[1,1])  # 1 row, 2 cols, 2nd subplot
 
-    # Example plot for the second subplot
-    # Replace this with your actual plotting code for the second plot
     x_EE = np.linspace(0, N*h, N+1)
     x_IM = np.linspace(0, N*h, N+1)
     ax2_1.plot(x_EE, np.abs(H(sol_EE)-H(y0)), 'k--',label="Explicit Euler")
